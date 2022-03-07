62f6349a2675f6fac20571fa46e5b4d733f81e43
tpnadeau <tyrail311@gmail.com>
2022-03-06 19:13:00 -0500

    add new functionality

--- app/src/main/java/com/example/hw3_to_do_list/AddItems.kt
+++ app/src/main/java/com/example/hw3_to_do_list/AddItems.kt
@@ -5,12 +5,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add_items.*
 
 class AddItems : AppCompatActivity() {
-    val REQUEST_CODE = 414
-    val todoList = arrayListOf<String>()
-
+    private val todoList = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +19,40 @@ class AddItems : AppCompatActivity() {
     fun goBackWithoutSaving(view : View)
     {
         val listEntryIntent = Intent(this, MainActivity::class.java)
-        startActivityForResult(listEntryIntent, REQUEST_CODE)
         setResult(Activity.RESULT_CANCELED, listEntryIntent)
         finish()
     }
 
     fun saveAndAdd(view : View)
     {
-        addToList()
-
+        if(write_a_task_text.text.isEmpty())
+            Toast.makeText(this, "Task cannot be empty. Please enter a task.", Toast.LENGTH_SHORT).show()
+        else {
+            addToList()
+            clearText()
+        }
     }
 
     fun goBackAndSave(view : View)
     {
-        val listEntryIntent = Intent(this, MainActivity::class.java)
-        addToList()
-        listEntryIntent.putExtra("tasks", todoList)
-        setResult(Activity.RESULT_OK, listEntryIntent)
-        finish()
+        if(write_a_task_text.text.isEmpty())
+            Toast.makeText(this, "Task cannot be empty. Please enter a task.", Toast.LENGTH_SHORT).show()
+        else {
+            val listEntryIntent = Intent(this, MainActivity::class.java)
+            addToList()
+            listEntryIntent.putExtra("tasks", todoList)
+            setResult(Activity.RESULT_OK, listEntryIntent)
+            finish()
+        }
     }
 
     fun addToList()
     {
         todoList.add(write_a_task_text.text.toString())
+    }
+
+    fun clearText()
+    {
+        write_a_task_text.text.clear()
     }
 }
--- app/src/main/java/com/example/hw3_to_do_list/MainActivity.kt
+++ app/src/main/java/com/example/hw3_to_do_list/MainActivity.kt
@@ -4,15 +4,19 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val REQUEST_CODE = 414
-    val newTodoArray = arrayListOf<String>()
-
+    private val REQUEST_CODE = 414
+    private val FILE_NAME = "MyToDoList"
+    private val newTodoArray = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,16 +24,24 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onStart()
     {
+        loadJson()
         super.onStart()
         val todoListAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, newTodoArray)
         list_view.adapter = todoListAdapter
         list_view.setOnItemLongClickListener { parent, view, position, id ->
             val selectedItem = parent.getItemAtPosition(position).toString()
-            Toast.makeText(this, "This is a long press, Deleting $selectedItem", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Completed: $selectedItem", Toast.LENGTH_SHORT).show()
             newTodoArray.removeAt(position)
+            if (newTodoArray.size == 0)
+                Toast.makeText(this, "All tasks are completed!", Toast.LENGTH_SHORT).show()
             todoListAdapter.notifyDataSetChanged()
             return@setOnItemLongClickListener true
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        saveJson()
     }
     fun openSecondActivity(view : View)
     {
@@ -44,6 +56,45 @@ class MainActivity : AppCompatActivity() {
         {
             for (task: String in todoList) {
                 newTodoArray.add(task)
+            }
+            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun saveJson() {
+
+        if (newTodoArray.size == 0) { // Make sure the list has some items
+            Toast.makeText(this, "Nothing to save", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val sharedPreferences = getSharedPreferences(FILE_NAME, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        val gson = Gson()
+        val newToDoArrayJson = gson.toJson(newTodoArray)
+        editor.putString("tasks", newToDoArrayJson)
+        editor.apply()
+
+        val toast = Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+
+
+    }
+
+
+    fun loadJson() {
+
+        val sharedPreferences = getSharedPreferences(FILE_NAME, MODE_PRIVATE)
+        val tasks = sharedPreferences.getString("tasks", "") ?: ""
+
+        if (tasks.isNotEmpty()){
+
+            val gson = Gson()
+            val sType = object : TypeToken<List<String>>() { }.type
+            val savedVehicleList = gson.fromJson<List<String>>(tasks, sType)
+
+            newTodoArray.clear()
+
+            for (vehicle in savedVehicleList) {
+                newTodoArray.add(vehicle)
             }
         }
     }
